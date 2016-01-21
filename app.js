330526346bbfa0b8b89ca30d64026334467d5aa9
Marek Kolodziej <marek.kolodziej@gonitro.con>
2016-01-20 21:50:32 -0800

    Factory

--- app.js
+++ app.js
@@ -1,16 +1,17 @@
-var app = angular.module('flapperNews', []);
+var app = angular.module('shreddit', []);
+
+app.factory('posts', [function(){
+  var o = {
+    posts: [{title: 'googol', upvotes: 5, link: 'http://www.google.com'}]
+  };
+  return o;
+}]);
 
 app.controller('MainCtrl', [
-'$scope',
-function($scope){
+'$scope', 'posts',
+function($scope, posts){
   $scope.test = 'Hello world!';
-  $scope.posts = [
-    {title: 'post 1', upvotes: 5},
-    {title: 'post 2', upvotes: 2},
-    {title: 'post 3', upvotes: 15},
-    {title: 'post 4', upvotes: 9},
-    {title: 'post 5', upvotes: 4}
-  ];
+  $scope.posts = posts.posts;
   $scope.addPost = function() {
   	if (!$scope.title || $scope.title === '') { return; }
   	$scope.posts.push({title: $scope.title, link: $scope.link, upvotes: 0});
@@ -21,16 +22,3 @@ function($scope){
     post.upvotes += 1;
   };
 }]);
-
-/*
-$scope.addPost = function(){
-  if(!$scope.title || $scope.title === '') { return; }
-  $scope.posts.push({
-    title: $scope.title,
-    link: $scope.link,
-    upvotes: 0
-  });
-  $scope.title = '';
-  $scope.link = '';
-};
-*/
